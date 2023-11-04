9d936c571e3754d7016c36919cb6e0299e11f8c2
Bakhbyergyen Yerjan <95435746+bakhbyergyen7@users.noreply.github.com>
2023-11-03 21:47:13 -0500

    Add files via upload

---
+++ HW1/cognitive_model_addition.m
@@ -0,0 +1,24 @@
+function result = cognitive_model_addition(int1, int2)
+    % Input processing
+    num1 = int32(int1);
+    num2 = int32(int2);
+
+    % Arithmetic
+    result = num1 + num2;
+
+    % Randomness
+    if rand() < 0.15
+        result = result + randi([-5, 5], 1);
+    end
+
+    % Timing
+    delay = randi([5, 60], 1);
+    tic;
+    for j = 1: delay
+        for i = 1:1000000000
+            i = i;
+        end
+    end
+    total_time = toc;
+    fprintf('It took %f seconds to compute the result\n',total_time);
+end
---
+++ HW1/test.m
@@ -0,0 +1,5 @@
+% Test the model
+int1 = 751235;
+int2 = 758768;
+result = cognitive_model_addition(int1, int2);
+fprintf('The result of adding %d and %d is: %d\n', int1, int2, result);
